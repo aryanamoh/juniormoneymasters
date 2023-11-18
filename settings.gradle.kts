7b3ad6b19cb52dcd4fcdfb81c47edf6ff7a35830
Aryana Mohammadi <am5723@barnard.edu>
2023-11-18 13:36:45 -0500

    base app

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Junior_Money_Masters"
+include(":androidApp")
+include(":shared")
